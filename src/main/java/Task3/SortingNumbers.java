57388d9cb468c90c18365ab0009a932158a96311
Ypppich <grystme@gmail.com>
2023-03-25 06:55:31 +0200

    Task3
    Sorting numbers

---
+++ src/main/java/Task3/SortingNumbers.java
@@ -0,0 +1,24 @@
+package Task3;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class SortingNumbers {
+    public static String arraySortingNumbers (String[] numbers) {
+        String result = Arrays.stream(numbers) //Створюємо змінну result у яку будуть записуватись данні з потоку.
+                .flatMapToInt(i -> Arrays.stream(i.split(",\\s+")) //Розділення рядків масиву numbers та конвертація їх в числа, щоб відсортувати
+                    .mapToInt(Integer::parseInt))
+                .sorted() //Сортування.
+                .mapToObj(String::valueOf) //Перетворення назад до рядка.
+                .collect(Collectors.joining(", ")); //Виводимо данні з потоку у result.
+        return result;
+    }
+}
+
+
+class SortingNumbersTest {
+    public static void main(String[] args) {
+        String[] numbers = {"1, 2, 0", "4, 5"};
+        System.out.println(new SortingNumbers().arraySortingNumbers(numbers));
+    }
+}
